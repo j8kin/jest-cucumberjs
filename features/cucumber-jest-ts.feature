407bbc7d529c9e1c29e2cd0eddec1a38f9b0734f
j8kin <emk161278@gmail.com>
2019-02-17 21:52:05 +0300

    Add cucumber related files. Update .gitignore to ignore autogenerated file

---
+++ features/cucumber-jest-ts.feature
@@ -0,0 +1,5 @@
+Feature: Cucumber Jest TS example
+
+   This feature file contain an example of the test with Cucumber Jest TS environment
+   Scenario: Simple scenario
+   Given a calculator
---
+++ features/steps/divideSteps.ts
@@ -0,0 +1,15 @@
+//import { expect } from "jest";
+import { Calculator } from "../../src/calculator";
+import { Given, When, Then } from "cucumber";
+ 
+Given("a calculator", function() {
+    throw new Error("Not implemented");
+});
+ 
+When("I divide {int} by {int}", function(n1: number, n2: number) {
+    throw new Error("Not implemented");
+});
+ 
+Then("the result is {int}", function(expected: number) {
+    throw new Error("Not implemented");
+});
